6f271aa825032186c9248a17771adafec4d8a438
Songday <songday@yeah.net>
2021-01-21 18:38:22 +0800

    升级到Tokio1

--- backend/src/rt.rs
+++ backend/src/rt.rs
@@ -1,12 +1,11 @@
 use tokio::runtime::{Builder, Runtime};
 
 pub fn init_runtime() -> Runtime {
-    let r = Builder::new()
-        .threaded_scheduler()
-        .enable_all()
-        .core_threads(num_cpus::get() + 1)
+    let r = Builder::new_multi_thread()
+        .worker_threads(num_cpus::get() + 1)
         .thread_name("songday-spider-rs")
-        .thread_stack_size(64 * 1024 * 1024)
+        .thread_stack_size(64 * 1024)
+        .enable_all()
         .build();
     match r {
         Ok(rt) => rt,
